86ece30a8009c1f7a4176ae376f3265eb4aec189
Hanna-Bk <appbesemab@gmail.com>
2023-02-16 17:16:04 -0800

    1-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program is about puzzle
+ *
+ * Return: return 0 success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
