702989f0ed67e6a9c7801ba4296e9ad14ae27600
Hanna-Bk <appbesemab@gmail.com>
2023-02-16 18:38:06 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,5 +3,11 @@
 /**
  * main - print string in put function
  *
- * Description: using the main function this program prints "and that piece of art is useful - Dora Korpar
- * Return 
+ * Description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is usefull\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
