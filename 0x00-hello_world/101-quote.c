ad0164f989b2857e2cd88c29cdc41a77277fee05
Hanna-Bk <appbesemab@gmail.com>
2023-02-16 18:16:08 -0800

    task 6.2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print string in put function
+ *
+ * Description: using the main function this program prints "and that piece of art is useful - Dora Korpar
+ * Return 
