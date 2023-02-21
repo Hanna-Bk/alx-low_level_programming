a031a42d18b72bbe5961b375aa28a3d3eabaa206
Hanna-Bk <appbesemab@gmail.com>
2023-02-21 15:04:10 -0800

    correct collen

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,10 @@
 void print_alphabet(void)
 {
 	char h;
+
 	for (h = 'a';h <= 'z'; h++);
 	{
 		_putchar(h);
 	}
-	_putchar('\n';)
+	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print alphabet ten times followed ny new line
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char h, b;
+	for (b = 0; b<= 9; b++);
+
+	{
+		for (h = 'a' ; h <= 'z'; h++);
+
+		{
+			_putchar(h);
+		}
+		_putchar('\n');
+	}
+}
