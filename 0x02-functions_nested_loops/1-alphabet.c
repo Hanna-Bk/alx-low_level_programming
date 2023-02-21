6495cd801554114ef09bcba17520ac0707bca700
Hanna-Bk <appbesemab@gmail.com>
2023-02-21 14:42:46 -0800

    low level pro. loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - the program that will print lowercase alphabets
+ *
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char h;
+	for (h = 'a';h <= 'z'; h++);
+	{
+		_putchar(h);
+	}
+	-putchar('\n';)
+}
