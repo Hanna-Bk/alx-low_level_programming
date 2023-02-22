1d860a6eaa5680c6aa2003a19b3ea00e77e10c7e
Hanna-Bk <appbesemab@gmail.com>
2023-02-21 17:00:50 -0800

    taks 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,4 +10,15 @@ void print_to_98(int n)
 {
 	while (n > 98)
 	{
-		printf
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	if (n == 98)
+		printf("%d", n);
+	printf("\n");
+}
