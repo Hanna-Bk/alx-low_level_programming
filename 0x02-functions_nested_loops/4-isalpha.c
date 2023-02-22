959be49887e271f738430939619fadc2084a0bca
Hanna-Bk <appbesemab@gmail.com>
2023-02-21 16:08:02 -0800

    clear white space

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - function to check if c is a letter, lowercase or uppercase
+ * _isalpha - function to check if c is a letter, lowercase or uppercase
  * Return: 0
  */
 int _isalpha(int c)
 {
 	char c;
-	
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 
 	{
 		return (1);
