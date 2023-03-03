60683ffaf45b501e04417d01f182544d31e7d9dc
Hanna-Bk <appbesemab@gmail.com>
2023-03-02 18:48:34 -0800

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,17 +5,17 @@
  * @s: string to make upper
  * Return: return value of upper
  */
- char *string_toupper(char *str)
-  9 {
- 10         int index = 0;
- 11
- 12         while (str[index])
- 13         {
- 14                 if (str[index] >= 'a' && str[index] <= 'z')
- 15                         str[index] = str[index] - 32;
- 16
- 17                 index++;
- 18         }
- 19
- 20         return (str);
- 21 }
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] = str[index] - 32;
+
+		index++;
+	}
+
+	return (str);
+}
