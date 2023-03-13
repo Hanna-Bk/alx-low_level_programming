d143b8941e9ed786802bfee2dcb751eb678d0303
Hanna-Bk <appbesemab@gmail.com>
2023-03-13 03:21:04 -0700

    creat static lib

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value
+ * @d: input for absoulte value
+ * Return: absolute value
+ */
+int _abs(int d)
+{
+	if (d > 0  ||  d == 0)
+	{
+	return (d);
+	}
+	else
+	return (d * -1);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,3 @@
+ Static libraries
+1) Create the static library 
+2) Create a script 
