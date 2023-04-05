a01976b20b4ba68462204ba1aa68c07d782ce368
Hanna-Bk <appbesemab@gmail.com>
2023-04-05 16:17:22 -0700

    create dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,19 +3,19 @@
 
 /**
  * struct dog - structure dog
- * @name: atribute pointer to chr nam's dog 
+ * @name: atribute pointer to char name's dog
  * @age: atribute float age's dog
  * @owner: atribute pointer to char owner's dog
  */
 
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
 
-typedef truct dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
